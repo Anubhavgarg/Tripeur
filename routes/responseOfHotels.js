32be92c46a6af38048df5f2f6d44cb5fb41e1193
Anubhav Garg <anubhav.g@darwinbox.io>
2018-06-21 00:52:34 +0530

    assignment of hotel prices

--- routes/responseOfHotels.js
+++ routes/responseOfHotels.js
@@ -3,35 +3,32 @@
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
 
+
 module.exports = async function(req, res){
+  const requestBody = req.body;
+  const {hotelName,checkInDate,duration} = requestBody;
 
-    const URL = "https://www.google.com/search?q=Taj West End, Bengaluru&ahotel_dates=2018-07-25%2C2#ahotel_dates=2018-07-25,2";
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(URL);
-
-
-    const divLenght = await page.$eval('div[data-async-type="updateHotelBookingModule"]', divs => divs.innerHTML);
-    const $ = cheerio.load(divLenght);
-
-    const hotelList = $('a[data-dp]');
-
-    const hotelDetails = Object.keys(hotelList).reduce((acc, key) => {
-      const elementIndex = parseInt(key);
-      if(elementIndex == key) {
-        const hotelSpan = $(`a[data-dp]>img`);
-        const hotel = hotelList[key];
-        acc.push({
-          price: hotel.attribs['data-dp'],
-          url: hotel.attribs['href'],
-          name: hotelSpan[key].attribs.alt
-        });
-      }
-      return acc;
-
-    }, []);
-    res.send(hotelDetails);
-// await page.screenshot({path: 'example.png'});
-    //console.log(divLenght);
-    await browser.close();
+  const url = `https://www.google.com/search?q=${hotelName}&ahotel_dates=${checkInDate}%2C${duration}#ahotel_dates=${checkInDate},${duration}`;
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+  const divLength = await page.$eval('div[data-async-type="updateHotelBookingModule"]', divs => divs.innerHTML);
+  const $ = cheerio.load(divLength);
+  const hotelList = $('a[data-dp]');
+  const hotelDetails = Object.keys(hotelList).reduce((acc, key) => {
+    const elementIndex = parseInt(key);
+    if(elementIndex == key) {
+      const hotelSpan = $(`a[data-dp]>img`);
+      const hotel = hotelList[key];
+      acc.push({
+        id: key,
+        price: hotel.attribs['data-dp'],
+        siteURL: hotel.attribs['href'],
+        siteName: hotelSpan[key].attribs.alt
+      });
+    }
+    return acc;
+  }, []);
+  res.send(hotelDetails);
+  await browser.close();
 };
