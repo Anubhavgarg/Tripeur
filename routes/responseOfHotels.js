2baadbd2c297dd9594a93abd2c3e19b447060ab6
Anubhav Garg <anubhav.g@darwinbox.io>
2018-06-21 01:24:54 +0530

    changes in teh code for small error handling

--- routes/responseOfHotels.js
+++ routes/responseOfHotels.js
@@ -12,23 +12,28 @@ module.exports = async function(req, res){
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url);
-  const divLength = await page.$eval('div[data-async-type="updateHotelBookingModule"]', divs => divs.innerHTML);
-  const $ = cheerio.load(divLength);
-  const hotelList = $('a[data-dp]');
-  const hotelDetails = Object.keys(hotelList).reduce((acc, key) => {
-    const elementIndex = parseInt(key);
-    if(elementIndex == key) {
-      const hotelSpan = $(`a[data-dp]>img`);
-      const hotel = hotelList[key];
-      acc.push({
-        id: key,
-        price: hotel.attribs['data-dp'],
-        siteURL: hotel.attribs['href'],
-        siteName: hotelSpan[key].attribs.alt
-      });
-    }
-    return acc;
-  }, []);
-  res.send(hotelDetails);
+  let divlength = '';
+  try {
+    divlength = await page.$eval('div[data-async-type="updateHotelBookingModule"]', divs => divs.innerHTML);
+    const $ = cheerio.load(divlength);
+    const hotelList = $('a[data-dp]');
+    const hotelDetails = Object.keys(hotelList).reduce((acc, key) => {
+      const elementIndex = parseInt(key);
+      if(elementIndex == key) {
+        const hotelSpan = $(`a[data-dp]>img`);
+        const hotel = hotelList[key];
+        acc.push({
+          id: key,
+          price: hotel.attribs['data-dp'],
+          siteURL: hotel.attribs['href'],
+          siteName: hotelSpan[key].attribs.alt
+        });
+      }
+      return acc;
+    }, []);
+    res.send(hotelDetails);
+  } catch (e) {
+    res.send({data:"Please provide correct params"});
+  }
   await browser.close();
 };
